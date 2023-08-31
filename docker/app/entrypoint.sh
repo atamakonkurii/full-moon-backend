47398325bfb82aefeabd7cd30f939ca49ae11b65
atamakonkurii <morisudesu@gmail.com>
2023-09-01 00:06:48 +0900

    マイグレーション実行後にapp containerが起動し続けるようにした

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
 export POSTGRESQL_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_USER}?sslmode=disable
 
-migrate -database ${POSTGRESQL_URL} -path db/migrations up 1
+# migration
+migrate -database ${POSTGRESQL_URL} -path docker/db/migrations up
+
+# コンテナを起動し続けるためにshを実行
+/bin/sh
